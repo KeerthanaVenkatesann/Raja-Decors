57d203c8270f7aee19336f726c732106552aac5d
samarasam24 <samarasamtmk2003@gmail.com>
2024-09-11 21:51:33 +0530

    navbar changes

--- src/Component/Home/Header/Header.jsx
+++ src/Component/Home/Header/Header.jsx
@@ -1,41 +1,67 @@
 import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
-import { useState } from "react";
+import { useState } from "react"; 
 
-const navStyle = {
+// navList  style
+const navList = {
+    fontSize:'18px',
+    fontWeight:600,
+    fontFamily: "Nunito Sans",
+    // color:'#535362',
     color:'black',
-    backgroundColor:'white',
-};
-
-// nav menu custom style  
-const menuStyle = {
-    display:{xs:'none',sm:'flex'},
-    columnGap:5,
+    cursor:'pointer',  
+    '&:hover':{
+        color:'blue'
+    }
 };
 
 function Header() {
     const menuItem = ['Home','Service','About Us', 'Contact Us'];
-    const [ mobile, setMobile ] = useState(false);
+    const [ open, setOpen ] = useState(false);
 
-    const ExpandMenu = () => {
-        setMobile( prev => !prev);
+    // nav style
+    const navStyle = {
+        color:'black',
+        backgroundColor:'white', 
+        display: {xs:'grid',md:'flex'},
+        gridTemplateColumns:'auto auto',
+        mx:{xs:5,md:12},
+        padding: open && '10px 0px',       
+    };
+    // navMenu container  style  
+    const menuStyle = {
+        display: { xs: open ? 'grid' : 'none', md:'flex' },
+        columnGap: 8,
+        fontWeight: 800, 
+    };
+    // icon box container style
+    const iconBoxStyle = {
+        display:{xs:'flex',md:'none'},
+        justifyContent:'end'
     };
 
+    const ExpandMenu = () => {
+        setOpen( prev => !prev);
+    }; 
+
     return(
-        <AppBar sx={navStyle}>
-            <Toolbar sx={{mx:10}}>
-                <Typography sx={{flexGrow:1}}>Logo</Typography>
-                <IconButton 
-                sx={{display:{xs:'block',sm:'none'},}}
-                onClick={ExpandMenu}
-                >
-                   <MenuIcon />
-                </IconButton>                
-                <Box sx={menuStyle}>
+        <AppBar sx={{ backgroundColor: 'white' }} >
+            <Toolbar sx={ navStyle }>
+                <Typography sx={{ flexGrow: 1 }} >
+                    Logo
+                </Typography>
+                <Box sx={ iconBoxStyle } >
+                    <IconButton 
+                    onClick={ExpandMenu}
+                    >
+                    <MenuIcon />
+                    </IconButton> 
+                </Box>               
+                <Box sx={ menuStyle }>
                     {
                         menuItem.map(
                             (value) => (
-                                <Typography key={value}>
+                                <Typography sx={ navList } key={ value }>
                                     {value}
                                 </Typography>
                             )
